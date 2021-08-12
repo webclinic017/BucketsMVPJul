1123e4176c4a713db8072d5388fad258d30dd04b
Akash Gupta <akashgupta@Akashs-MacBook-Air.local>
2021-08-12 13:20:22 -0400

    meta tags and description

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ import 'toastr/build/toastr.min.css';
 import store from "./Redux";
 import Navigation from "./Navigation";
 import React from "react";
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 
 const App = () => {
   return (
     <Provider store={store}>
       <Navigation />
-      <div className="App">
-      <Helmet>
-        <title>Buckets Investing</title>
-        <meta name="Create a playlist of stocks today" content="Investing Simplified" />
-      </Helmet>
-    </div>
     </Provider>
   );
 }
