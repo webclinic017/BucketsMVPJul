8f96629e1835299e692bd57d9357346d7d8e0e00
Akash Gupta <akashgupta@Akashs-MacBook-Air.local>
2021-08-12 16:12:53 -0400

    fixing baseurl

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const APIClient = axios.create({
-  baseURL: 'https://buckets-server-v3.app.com'
+  baseURL: 'https://buckets-server-v3.herokuapp.com/'
   // baseURL: 'http://127.0.0.1:5000'
 });
 
