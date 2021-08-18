1840147ca308fe65c12eb50320d177c79806fb44
Akash Gupta <akashgupta@Akashs-Air.cogeco.local>
2021-08-18 09:23:16 -0400

    proxy headers fixed

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const APIClient = axios.create({
-  baseURL: 'https://floating-forest-52806.herokuapp.com/https://buckets-server-v3.herokuapp.com'
-  // baseURL: 'http://localhost:8080//http://127.0.0.1:5000'
+  // baseURL: 'https://floating-forest-52806.herokuapp.com/https://buckets-server-v3.herokuapp.com'
+  // baseURL: 'http://127.0.0.1:5000'
+  baseURL: 'https://buckets-server-v3.herokuapp.com'
 });
 
 export const insertTokenInHeaders = (token) => {
