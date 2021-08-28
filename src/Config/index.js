11badbe07149fd35ce5767c32c1a78618e5ed2de
Faizan Mostafa <faizanmustafa324@gmail.com>
2021-08-28 22:19:55 +0500

    alpaca rediretUri reupdated in config

--- src/Config/index.js
+++ src/Config/index.js
@@ -2,7 +2,7 @@ const config = {
   constants: {
     alpacaClientId: "73b2b400ce17d4a8c91e3f4b4e776cdf",
     alpacaState: "749enib@erbr&7r9392&%023no!i480n9n",
-    redirectUri: "https://www.bucketsinvesting.com/"
+    redirectUri: "https://www.bucketsinvesting.com/authorize-alpaca"
   }
 };
 
