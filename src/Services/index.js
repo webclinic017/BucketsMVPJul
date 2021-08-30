d499825c3b41fafb71fba985c91f764acab95501
Faizan Mostafa <faizanmustafa324@gmail.com>
2021-08-30 18:10:05 +0500

    baseUrl updated in APIClient

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const APIClient = axios.create({
-  baseURL: 'https://floating-forest-52806.herokuapp.com/https://buckets-server-v3.herokuapp.com'
+  // baseURL: 'https://floating-forest-52806.herokuapp.com/https://buckets-server-v3.herokuapp.com'
   // baseURL: 'http://127.0.0.1:5000'
-  // baseURL: 'https://buckets-server-v3.herokuapp.com'
+  baseURL: 'https://buckets-server-v3.herokuapp.com'
 });
 
 export const insertTokenInHeaders = (token) => {
