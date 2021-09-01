a5c1f76bc77738ce229ca5b2e7dd43dd27f46666
Faizan Mostafa <faizanmustafa324@gmail.com>
2021-09-01 21:24:02 +0500

    Portfolio index.js fixed

--- src/Screens/Portfolio/index.js
+++ src/Screens/Portfolio/index.js
@@ -184,46 +184,49 @@ const Portfolio = (props)=> {
         setBucketBeta(beta);
         setBucketSize(size);
       }
-    }
-    
-    let valueArray = []
-    console.log(bucketHistoricalPrices)
-    
-    bucketData.stocks.map(stock => { 
-      console.log(stock)
-      console.log(stock.targetWeight)
-      if (stock.targetWeight>0){
-        const amountInvested = 10000 * (stock.targetWeight/100)
-        const ticker = stock.ticker
-        stock.numberDummyShares = bucketHistoricalPrices[ticker] ? amountInvested/(bucketHistoricalPrices[ticker][bucketHistoricalPrices[ticker].length - 1].close) : 'FAILSAFE-VAULE';
-        
-        if (typeof bucketHistoricalPrices[ticker] !== 'undefined') {
-          bucketHistoricalPrices[ticker].forEach((price) => {
-            price.value = stock.numberDummyShares * price.close
-            price.date = new Date(price.date).getTime()
-            valueArray.push(price)
-          })
-        }
-      }
-    });
-
-    const res = Array.from(valueArray.reduce(
-      (m, {date, value}) => m.set(date, (m.get(date) || 0) + value), new Map
-    ), ([date, value]) => ({date, value}));
-    // console.log(res);
-
-    var outputData = res.map( Object.values );
-    const gdata = outputData.reverse();
-    console.log(gdata);
-    setGData(gdata)
-
-    // console.log(valueArray)
-    // for (const [key, value] of Object.entries(res)) {
-    //   const map = value.map((entry)=>([entry.date, entry.value]));
-    //   return map.reverse()
-    // }
-      
+    } 
   }, [bucketData]);
+
+  useEffect(()=>{
+    if(stocks.length && bucketHistoricalPrices) {
+      let valueArray = []
+      console.log(bucketHistoricalPrices)
+      
+      bucketData.stocks.map(stock => { 
+        console.log(stock)
+        console.log(stock.targetWeight)
+        if (stock.targetWeight>0){
+          const amountInvested = 10000 * (stock.targetWeight/100)
+          const ticker = stock.ticker
+          stock.numberDummyShares = bucketHistoricalPrices[ticker] ? amountInvested/(bucketHistoricalPrices[ticker][bucketHistoricalPrices[ticker].length - 1].close) : 'FAILSAFE-VAULE';
+          
+          if (typeof bucketHistoricalPrices[ticker] !== 'undefined') {
+            bucketHistoricalPrices[ticker].forEach((price) => {
+              price.value = stock.numberDummyShares * price.close
+              price.date = new Date(price.date).getTime()
+              valueArray.push(price)
+            })
+          }
+        }
+      });
+
+      const res = Array.from(valueArray.reduce(
+        (m, {date, value}) => m.set(date, (m.get(date) || 0) + value), new Map
+      ), ([date, value]) => ({date, value}));
+      // console.log(res);
+
+      var outputData = res.map( Object.values );
+      const gdata = outputData.reverse();
+      console.log(gdata);
+      setGData(gdata)
+
+      // console.log(valueArray)
+      // for (const [key, value] of Object.entries(res)) {
+      //   const map = value.map((entry)=>([entry.date, entry.value]));
+      //   return map.reverse()
+      // }
+    }
+  }, [stocks, bucketHistoricalPrices]);
 
   const handleOnClickBucketShare = () => {
     setShareModalVisibility(true);
